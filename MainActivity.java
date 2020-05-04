8204091dbc86e58f05a340f388471058b2600f73
xuehaipiaoxiang <56183476+xuehaipiaoxiang@users.noreply.github.com>
2020-05-04 17:27:25 +0800

    Custom AlertDialog

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.application4;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,22 +27,48 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast=new Toast(getApplicationContext());
+//                Toast toast=new Toast(MainActivity.this);
+//
+//                // set View
+//                LayoutInflater layoutInflater=LayoutInflater.from(MainActivity.this);
+//                View view=layoutInflater.inflate(R.layout.toast_view,null);
+//
+//                //set details
+//                ImageView imageView=view.findViewById(R.id.imageView);
+//                TextView textView=view.findViewById(R.id.textView);
+//                imageView.setImageResource(R.drawable.z5);
+//                textView.setText("hello ");
+//
+//                toast.setView(view);
+//                toast.setGravity(Gravity.CENTER,0,0);
+//                toast.setDuration(Toast.LENGTH_SHORT);
+//                toast.show();
+//            ToastUtil.showMsg(MainActivity.this,"hello");
 
-                // set View
-                LayoutInflater layoutInflater=LayoutInflater.from(MainActivity.this);
-                View view=layoutInflater.inflate(R.layout.toast_view,null);
 
-                //set details
-                ImageView imageView=view.findViewById(R.id.imageView);
-                TextView textView=view.findViewById(R.id.textView);
-                imageView.setImageResource(R.drawable.z5);
-                textView.setText("hello ");
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                builder.setTitle("Tiltle").setMessage("Main Message").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        ToastUtil.showMsg(MainActivity.this,"ok");
+//                    }
+//                }).show();
 
-                toast.setView(view);
-                toast.setGravity(Gravity.CENTER,0,0);
-                toast.setDuration(Toast.LENGTH_SHORT);
-                toast.show();
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                final View view=LayoutInflater.from(MainActivity.this).inflate(R.layout.alert_view,null);
+                Button button2=view.findViewById(R.id.button2);
+                button2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TextView textView =view.findViewById(R.id.editText);
+                        String str =textView.getText().toString();
+                        Log.e(str,str);
+                    }
+                });
+                builder.setView(view);
+                builder.setTitle("Cus");
+                builder.show();
+                //builder.setPositiveButton("")
 
             }
         });
