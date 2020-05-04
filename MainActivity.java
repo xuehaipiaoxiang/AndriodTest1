f56e94410bdd0074188dc0fa159feda854d6cbc7
xuehaipiaoxiang <56183476+xuehaipiaoxiang@users.noreply.github.com>
2020-05-04 14:31:46 +0800

    TOAST custom

--- MainActivity.java
+++ MainActivity.java
@@ -1,75 +1,48 @@
-package com.example.mybmi_2;
+package com.example.application4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private EditText height, weight, name;
-    private Button computing, reset;
-    private RadioGroup sex;
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        initMyCpnt();
-    }
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast=new Toast(getApplicationContext());
 
-    @Override
-    public void onClick(View v) {
-        int i=v.getId();
-        switch (i){
-            case R.id.idCmp:
-                Bundle ans=computing();
-                emitMsg(ans);
-                break;
-            case R.id.idRST:
-                Toast.makeText(this,"reset",Toast.LENGTH_SHORT);
-                break;
+                // set View
+                LayoutInflater layoutInflater=LayoutInflater.from(MainActivity.this);
+                View view=layoutInflater.inflate(R.layout.toast_view,null);
 
-             default:
+                //set details
+                ImageView imageView=view.findViewById(R.id.imageView);
+                TextView textView=view.findViewById(R.id.textView);
+                imageView.setImageResource(R.drawable.z5);
+                textView.setText("hello ");
 
-        }
-    }
+                toast.setView(view);
+                toast.setGravity(Gravity.CENTER,0,0);
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.show();
 
-    private void initMyCpnt(){
-
-        name=findViewById(R.id.idName);
-        height=findViewById(R.id.idHeight);
-        weight=findViewById(R.id.idWeight);
-        computing=findViewById(R.id.idCmp);
-        reset=findViewById(R.id.idRST);
-        sex=findViewById(R.id.idBGroup);
-
-        computing.setOnClickListener(this);
-        reset.setOnClickListener(this);
-
+            }
+        });
 
     }
-    private void emitMsg(Bundle bundle){
-        Intent intent=new Intent();
-        intent.setClass(this,Second.class);
-        intent.putExtras(bundle);
-        startActivity(intent);
 
-    }
-    private Bundle computing(){
-        float numH=Float.parseFloat(height.getText().toString());
-        float numW=Float.parseFloat(weight.getText().toString());
-        String _name=name.getText().toString();
 
-        Bundle bundle=new Bundle();
-        float ans =numW/numH*numH;
-        bundle.putString("name",_name);
-        bundle.putFloat("bmi",ans);
-        return  bundle;
-    }
 }
